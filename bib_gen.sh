d00f84f34e6fb72ec0112b3ab3167418c80dcdc6
Shoichi Koyama <koyama.shoichi@ieee.org>
2024-03-20 07:34:09 +0900

    update

--- bib_gen.sh
+++ bib_gen.sh
@@ -1,7 +1,7 @@
 # Web
-bib2bib -c 'year >= 2023' --expand --no-comment --remove funding --remove award_ja -ob ./web/skoyamalab_en.bib str_def_full.bib skoyamalab_en.bib
-bib2bib -c '(year >= 2023) and not ($key : "_en$")' --expand --no-comment --remove funding --remove award_en -ob ./web/skoyamalab_ja.bib str_def_full.bib skoyamalab_en.bib skoyamalab_ja.bib
+bib2bib -c 'year >= 2023' --expand --no-comment --remove funding --remove award_ja -ob ./web/skoyamalab_en.bib str_def_full.bib data_en.bib
+bib2bib -c '(year >= 2023) and not ($key : "_en$")' --expand --no-comment --remove funding --remove award_en -ob ./web/skoyamalab_ja.bib str_def_full.bib data_en.bib data_ja.bib
 
 # BIB
-bib2bib --remove abbr --remove preview --remove html --remove arxiv --remove funding --remove award_ja --remove award_en --remove category --remove pdf --remove slides --remove lang --remove video --remove bibtex_show --remove google_scholar_id --remove IEICEnotice -s year -ob ./bib/skoyamalab_en.bib skoyamalab_en.bib
-bib2bib -c 'not ($key : "_en$")'  --remove abbr --remove preview --remove html --remove arxiv --remove funding --remove award_ja --remove award_en --remove category --remove pdf --remove slides --remove lang --remove video --remove bibtex_show --remove google_scholar_id --remove IEICEnotice -s year -ob ./bib/skoyamalab_ja.bib skoyamalab_en.bib skoyamalab_ja.bib
+bib2bib --remove abbr --remove preview --remove html --remove arxiv --remove funding --remove award_ja --remove award_en --remove category --remove pdf --remove slides --remove lang --remove video --remove bibtex_show --remove google_scholar_id --remove IEICEnotice -s year -ob ./bib/skoyamalab_en.bib data_en.bib
+bib2bib -c 'not ($key : "_en$")'  --remove abbr --remove preview --remove html --remove arxiv --remove funding --remove award_ja --remove award_en --remove category --remove pdf --remove slides --remove lang --remove video --remove bibtex_show --remove google_scholar_id --remove IEICEnotice -s year -ob ./bib/skoyamalab_ja.bib data_en.bib data_ja.bib
