09d25ea54e9cb35b0cc9a2ff443ecc79cd5ccc44
Shoichi Koyama <koyama.shoichi@ieee.org>
2024-03-20 00:23:53 +0900

    update

--- bib_gen.sh
+++ bib_gen.sh
@@ -1,2 +1,7 @@
-bib2bib --expand --no-comment --remove funding --remove award_ja -s '$data' -ob ./koyama_en_js.bib str_def_full.bib pub_ks_en.bib
-bib2bib -c 'not ($key : "_en$")' --expand --no-comment --remove funding --remove award_en -s '$data' -ob ./koyama_ja_js.bib str_def_full.bib pub_ks_ja.bib pub_ks_en.bib
+# Web
+bib2bib -c 'year >= 2023' --expand --no-comment --remove funding --remove award_ja -ob ./web/skoyamalab_en.bib str_def_full.bib skoyamalab_en.bib
+bib2bib -c '(year >= 2023) and not ($key : "_en$")' --expand --no-comment --remove funding --remove award_en -ob ./web/skoyamalab_ja.bib str_def_full.bib skoyamalab_en.bib skoyamalab_ja.bib
+
+# BIB
+bib2bib --remove abbr --remove preview --remove html --remove arxiv --remove funding --remove award_ja --remove award_en --remove category --remove pdf --remove slides --remove lang --remove video --remove bibtex_show --remove google_scholar_id --remove IEICEnotice -s year -ob ./bib/skoyamalab_en.bib skoyamalab_en.bib
+bib2bib -c 'not ($key : "_en$")'  --remove abbr --remove preview --remove html --remove arxiv --remove funding --remove award_ja --remove award_en --remove category --remove pdf --remove slides --remove lang --remove video --remove bibtex_show --remove google_scholar_id --remove IEICEnotice -s year -ob ./bib/skoyamalab_ja.bib skoyamalab_en.bib skoyamalab_ja.bib
