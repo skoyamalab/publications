5b92cfce6674a424ae24a71313f8664e98a9d73c
Shoichi Koyama <koyama.shoichi@ieee.org>
2024-03-20 21:57:52 +0900

    modified skotama

--- skoyama/bib_gen.sh
+++ skoyama/bib_gen.sh
@@ -1,3 +1,3 @@
-bib2bib -c 'author : "Koyama"' --expand --no-comment --remove funding --remove award_ja -ob ./skoyama_en.bib ../str_def_full.bib ../data_en.bib
-bib2bib -c 'author : "Koyama" and not ($key : "_en$")' --expand --no-comment --remove funding --remove award_en -ob ./skoyama_ja.bib ../str_def_full.bib ../data_en.bib ../data_ja.bib
+bib2bib -c 'author : "Shoichi Koyama"' --expand --no-comment --remove funding --remove award_ja -ob ./skoyama_en.bib ../str_def_full.bib ../data_en.bib
+bib2bib -c '(author : "Shoichi Koyama" or author : "小山 翔一") and not ($key : "_en$")' --expand --no-comment --remove funding --remove award_en -ob ./skoyama_ja.bib ../str_def_full.bib ../data_en.bib ../data_ja.bib
 
